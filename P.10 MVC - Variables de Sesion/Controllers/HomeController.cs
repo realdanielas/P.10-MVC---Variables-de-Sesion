7cb0fd65e775a31a80f7e3029702dd2b47e5f93d
realdanielas <daniela9.saavedra@gmail.com>
2024-05-26 17:32:45 -0600

    Add project files.

---
+++ P.10 MVC - Variables de Sesion/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using P._10_MVC___Variables_de_Sesion.Models;
+using System.Diagnostics;
+
+namespace P._10_MVC___Variables_de_Sesion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ P.10 MVC - Variables de Sesion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P._10_MVC___Variables_de_Sesion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
